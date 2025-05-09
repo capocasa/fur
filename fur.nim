727918f721f701514089e3b5acdfc6a90071ff32
Carlo Capocasa <carlo@capocasa.net>
2025-05-09 15:08:31 +0200

    working nim native

--- fur.nim
+++ fur.nim
@@ -1,25 +1,80 @@
+import std/[math]
 
-{.compile: "rtfir.c".}
-{.passl: "-lm" }
 type
-  Fur* = ref object
-    coeff*: ptr float
-    buffer*: ptr float
-    taps*: cuint
+  Fur* = object
+    coeff*: seq[float]
+    buffer*: seq[float]
+    taps*: int
 
-proc initLowpass*(fur: Fur, taps: cuint, freq: float) {.importc: "RTFIR_init_lowpass".}
-proc process*(fur: Fur, sample: float): float {.importc: "RTFIR_filter"}
+proc initFur*(taps: int): Fur =
+  result.coeff = newSeq[float](taps)
+  result.buffer = newSeq[float](taps)
+  result.taps = taps
+  result.coeff[0] = 1.0
 
-#bool RTFIR_init_lowpass(RTFIR *Filter,const unsigned int Taps,const double Freq);
-#bool RTFIR_init_highpass(RTFIR *Filter,const unsigned int Taps,const double Freq);
-#bool RTFIR_init_bandpass(RTFIR *Filter,const unsigned int Taps,const double Low,const double High);
-#bool RTFIR_init_bandstop(RTFIR *Filter,const unsigned int Taps,const double Low,const double High);
+template limit(freq: float)=
+  # todo nicer
+  assert freq >= 0.0 and freq <= 0.5
+  #if freq < 0.0:
+  #  0.0
+  #elif freq > 0.5:
+  #  0.5
+  #else:
+  #  freq
 
-#// Filters a sample with a FIR object
-#double RTFIR_filter(RTFIR *Filter,const double Sample);
+template lopass*(fur: var Fur, freq: float) =
+  let w = fur.taps div 2
+  fur.coeff[w] = 0.1
+  for i in -w..w-1:
+    if i == 0:
+      fur.coeff[w] = 2 * freq
+    else:
+      fur.coeff[i+w]=sin(2 * PI * freq * i.float) / (i.float * PI)
 
-#// Deletes a FIR object
-#void RTFIR_close(RTFIR *Filter);
+template lopass*(taps: int, freq: float) =
+  initFur(taps).lopass(float)
 
+template hipass*(fur: var Fur, freq: float) =
+  limit(freq)
+  let w = fur.taps div 2
+  for i in -w..w-1:
+    if i == 0:
+      fur.coeff[w] = 2 * freq
+    else:
+      fur.coeff[i+w] = -sin(2 * PI * freq * i.float) / (i.float * PI)
 
+template hipass*(taps: int, freq: float) =
+  initFur(taps).hipass(float)
+
+template bandpass*(fur: var Fur, hi: float, lo: float) =
+  limit(hi)
+  limit(lo)
+  let w = fur.taps div 2
+  for i in -w..w-1:
+    if i == 0:
+      fur.coeff[w] = ((2 * PI * hi) - (2 * PI * lo)) / PI
+    else:
+      fur.coeff[i+w]= (sin(2 * PI * hi * i.float) - sin(2 * PI * lo * i.float)) / (i.float * PI)
+
+template bandpass*(taps: int, freq: float) =
+  initFur(taps).bandpass(float)
+
+template notch*(fur: var Fur, hi: float, lo: float) =
+  limit(hi)
+  limit(lo)
+  let w = fur.taps div 2
+  for i in -w..w-1:
+    if i == 0:
+      fur.coeff[w] = 1 + ((2 * PI * lo) - (2 * PI * hi)) / PI
+    else:
+      fur.coeff[i+w] = (sin(2 * PI * lo * i.float) - sin(2 * PI * hi * i.float)) / (i.float * PI)
+
+template notch*(taps: int, freq: float) =
+  initFur(taps).notch(float)
+
+proc process*(fur: var Fur, sample: float): float =
+    copyMem(addr fur.buffer[1], addr fur.buffer[0], (fur.buffer.len-1) * sizeof(float))
+    fur.buffer[0]=sample
+    for i in 0..fur.taps-1:
+      result += fur.buffer[i] * fur.coeff[i]
 
